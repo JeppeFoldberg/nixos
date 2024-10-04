01db0bc4ab0893ba1443e1e9fcf73cf0577ac49b
Jeppe Foldberg <jeppe_05@msn.com>
2024-10-04 15:44:30 +0200

    now works with lock and flake!

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-  description = "My first simple NixOS flake!";
+  description = "My first simple flake";
 
   inputs = {
-    nixpgks.url = "github:NixOS/nixpgks/nixos-23.11";
+    # NixOS official package source, using the nixos-24.05 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
-  
-  outputs = { self, nixpgks, ... }@inputs: {
-    nixosConfigurations.nixos = nixpgks.lib.nixosSystem {
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
         ./configuration.nix
       ];
     };
