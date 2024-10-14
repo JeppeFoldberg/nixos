b1a492f5f494695dd187dbeb5392eec5f7d2b8c9
Jeppe Foldberg <jeppe_05@msn.com>
2024-10-14 20:38:57 +0200

    add configuration for screenlight buttons

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,16 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # screen light
+  programs.light.enable = true;
+  services.actkbd = {
+	  enable = true;
+	  bindings = [
+	  { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+	  { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+	  ];
+  };
+
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
