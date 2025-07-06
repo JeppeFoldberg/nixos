973047d4b99e76c2591e053fac350f5a031b0434
Jeppe Foldberg <jeppe_05@msn.com>
2025-07-06 10:39:05 +0200

    lower dpi-settings;not sure of correctness

--- home.nix
+++ home.nix
@@ -22,7 +22,7 @@
   # set cursor size and dpi for 4k monitor - should probably be changed! 
   xresources.properties = {
     "Xcursor.size" = 16;
-    "Xft.dpi" = 172;
+    "Xft.dpi" = 96;
   };
 
   # Packages that should be installed to the user profile.
